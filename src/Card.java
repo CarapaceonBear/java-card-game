17e6291ed22a3ec28648ca30ea8b6c6952d8ac29
carapaceonbear <seerhan17@gmail.com>
2022-07-05 11:35:52 +0100

    card class

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,27 @@
 public class Card {
+
+    private final String suit;
+    private final String symbol;
+    private final int value;
+
+    public Card(String suit, String symbol, int value) {
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return (symbol + suit);
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+    public String getSymbol() {
+        return symbol;
+    }
+    public int getValue() {
+        return value;
+    }
 }
