f21fa4382393f0e7eb05c30a85e42bde1f26c10c
carapaceonbear <seerhan17@gmail.com>
2022-07-05 11:55:01 +0100

    cardGame class, createDeck and getDeck methods

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardGame {
+
+    private String name;
+    String[] suits = {"\u2664", "\u2667", "\u2665", "\u2666",};
+    String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    List<Card> deck = new ArrayList<>();
+
+    public CardGame(String name) {
+        this.name = name;
+        createDeck();
+    }
+
+    public CardGame(){}
+
+    public void createDeck() {
+        for (String suit : suits) {
+            for (int i = 0; i < 13; i++) {
+                deck.add(new Card(suit, symbols[i], values[i]));
+            }
+        }
+    }
+
+    public String[] getDeck() {
+        String[] cardList = new String[52];
+        for (int i = 0; i < 52; i++) {
+            cardList[i] = deck.get(i).toString();
+        }
+        return cardList;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         UserInput user = new UserInput();
         Player playerOne = new Player();
         Player playerTwo = new Player();
-        Snap game = new Snap(playerOne, playerTwo);
+//        Snap game = new Snap(playerOne, playerTwo);
+        CardGame game = new CardGame("snap");
+        System.out.println(Arrays.toString(game.suits));
+        System.out.println(Arrays.toString(game.getDeck()));
 
 //        game.runGame();
     }
