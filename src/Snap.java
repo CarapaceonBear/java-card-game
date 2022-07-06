16d92818e16cf74d1279dbb5269eed314242d9d8
carapaceonbear <seerhan17@gmail.com>
2022-07-06 12:22:13 +0100

    timer not quite working

--- src/Snap.java
+++ src/Snap.java
@@ -1,8 +1,12 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Snap extends CardGame {
 
     String[] playerNames = new String[2];
     Card previousCard;
     Card currentCard;
+    boolean timerActive;
 
     public Snap(String name, UserInput user) {
         super.setName(name);
@@ -30,29 +34,50 @@ public class Snap extends CardGame {
             currentCard = dealCard();
             user.printMessage(currentCard.toString());
             if (currentCard.getSymbol().equals(previousCard.getSymbol())) {
-//                timer
-                if (user.getStringInput("It's a match!").equals("snap")) {
-                    if (turn == 0) {
-                        playerOne.incrementScore();
-                    } else {
-                        playerTwo.incrementScore();
-                    }
-                    user.printMessage(String.format("%s wins", playerNames[turn]));
-                } else {
-                    if (turn == 0) {
-                        playerTwo.incrementScore();
-                        user.printMessage(String.format("%s wins", playerNames[1]));
-                    } else {
-                        playerOne.incrementScore();
-                        user.printMessage(String.format("%s wins", playerNames[0]));
-                    }
-                }
                 isActive = false;
+                timerActive = true;
+                runTimer(turn);
+                parallelToTimer(turn);
             }
             if (turn == 0) {
                 turn = 1;
             } else {
                 turn = 0;
+            }
+        }
+        user.printMessage(String.format("%s : %d, %s : %d", playerOne.getName(), playerOne.getScore(), playerTwo.getName(), playerTwo.getScore()));
+        String choice = user.getStringInput("Play again? y/n");
+        if (choice.equals("y")) {
+            runGame();
+        }
+    }
+
+    private void runTimer(int turn) {
+        TimerTask task = new TimerTask() {
+            public void run() {
+                if (turn == 0) {
+                    playerTwo.incrementScore();
+                    user.printMessage(String.format("%s wins", playerNames[1]));
+                } else {
+                    playerOne.incrementScore();
+                    user.printMessage(String.format("%s wins", playerNames[0]));
+                }
+                timerActive = false;
+            }
+        };
+        Timer timer = new Timer("Timer");
+        long delay = 2000L;
+        timer.schedule(task, delay);
+    }
+    private void parallelToTimer(int turn) {
+        while (timerActive) {
+            if (user.getStringInput("It's a match!").equals("snap")) {
+                if (turn == 0) {
+                    playerOne.incrementScore();
+                } else {
+                    playerTwo.incrementScore();
+                }
+                user.printMessage(String.format("%s wins", playerNames[turn]));
             }
         }
     }
